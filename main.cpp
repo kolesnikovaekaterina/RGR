81fbe20170135bdc776c8f92cafedf354f02818a
kolesnikovaekaterina <62741892+kolesnikovaekaterina@users.noreply.github.com>
2020-05-29 22:00:49 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "field.h"
+#include <stdio.h>
+
+using namespace std;
+
+int main(void) {
+	Field a;
+	if (!a.read("map.txt"))
+		return 1;
+	a.way();
+	if (!a.is_way())
+		printf("Way is not exist\n");
+	else {
+		a.gen_way();
+		a.print();
+		printf("Answer> %d\n", a.length_way());
+	}
+	return 0;
+}
